50443ae848a94274b5d7c9b5a439a7738e0a355f
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-08-20 10:13:45 -0700

    Pull pet list state from api on load

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,24 @@
+import { useState, useEffect } from 'react';
+import * as petService from './services/petService';
+
 const App = () => {
+  const [petList, setPetList] = useState([]);
+
+  useEffect(() => {
+    const fetchPets = async () => {
+      try {
+        const pets = await petService.index();
+        if (pets.error) {
+          throw new Error(pets.error);
+        };
+        setPetList(pets);
+      } catch (err) {
+        console.log(err);
+      };
+    };
+    fetchPets();
+  }, []);
+
   return <h1>Hello world!</h1>
 };
 
